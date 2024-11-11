16a97313faaf235e2dd575e830421c70b76013dd
Irfandi <dlacia.vendy@gmail.com>
2024-11-11 17:31:17 +0700

    feat: simple sepolia contract

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,55 @@
 'use client';
 
+import useSepoliaTestContract from "@/hooks/useSepoliaTestContract";
 import { ConnectKitButton } from "connectkit";
+import { useState } from "react";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 
 export default function Home() {
+  const account = useAccount();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
+
+  const sepoliaTestContract = useSepoliaTestContract({ publicClient, walletClient });
+  const [number, setNumber] = useState<number>(0);
+  const [result, setResult] = useState<string>();
+
   return (
-    <div className="container mx-auto p-10 flex flex-col gap-4">
-      <div className="font-bold text-2xl">Wagmi - Safe</div>
-      <ConnectKitButton />
+    <div className="container mx-auto p-10 flex flex-col gap-4 items-start">
+      <div className="w-full flex justify-between">
+        <div className="font-bold text-2xl">Wagmi - Safe</div>
+        <ConnectKitButton showBalance={true} />
+      </div>
+      {account.isConnected &&
+        <>
+          <button
+            onClick={async () => {
+              setResult(undefined);
+              const _result = await sepoliaTestContract.retrieve();
+              setResult(JSON.stringify(_result));
+            }}
+            className="rounded-lg px-4 py-1 border-2 border-blue-500 text-blue-100 bg-blue-900 hover:bg-blue-800 transition-all font-bold text-sm"
+          >
+            Retrieve
+          </button>
+          <hr className="w-1/2 border border-slate-400" />
+
+          <input type="text" value={number} onChange={(val) => setNumber(Number(val.target.value))} className="text-black" />
+          <button
+            onClick={async () => {
+              setResult(undefined);
+              const _result = await sepoliaTestContract.store(number);
+              setResult(JSON.stringify(_result));
+            }}
+            className="rounded-lg px-4 py-1 border-2 border-blue-500 text-blue-100 bg-blue-900 hover:bg-blue-800 transition-all font-bold text-sm"
+          >
+            Store
+          </button>
+          {result !== undefined &&
+            <div>{result}</div>
+          }
+        </>
+      }
     </div>
   );
 }
---
+++ hooks/useSepoliaTestContract.ts
@@ -0,0 +1,68 @@
+import { PublicClient, WalletClient } from "viem";
+import { readContract } from "viem/actions";
+import sepoliaAbi from '../resources/sepolia-abi.json';
+
+interface Props {
+    publicClient?: PublicClient
+    walletClient?: WalletClient
+}
+
+const useSepoliaTestContract = (props: Props) => {
+    const contractAddress = '0x2331fb827792879D21e11f7e13bA0d57391393D5';
+
+    const retrieve = async () => {
+        console.log('retrieve');
+        try {
+            if (props.publicClient === undefined) {
+                throw 'Public client undefined';
+            }
+            const result = await readContract(props.publicClient, {
+                address: contractAddress,
+                abi: sepoliaAbi.abi,
+                functionName: 'retrieve',
+                args: [],
+            });
+            console.log('useSepoliaTestContract.retrieve', result);
+
+            return result?.toString();
+        } catch (error) {
+            console.error(error);
+            return undefined;
+        }
+    }
+
+    const store = async (value: Number) => {
+        console.log('store');
+        try {
+            if (props.publicClient === undefined) {
+                throw 'Public client undefined';
+            }
+            if (props.walletClient === undefined) {
+                throw 'Wallet client undefined';
+            }
+            const { request } = await props.publicClient.simulateContract({
+                account: props.walletClient.account,
+                address: contractAddress,
+                abi: sepoliaAbi.abi,
+                functionName: 'store',
+                args: [value]
+            })
+            const storeHash = await props.walletClient.writeContract(request);
+
+            const storeReceipt = await props.publicClient.waitForTransactionReceipt({ hash: storeHash });
+            console.log('Store receipt', storeReceipt);
+
+            return 'success';
+        } catch (error) {
+            console.error(error);
+            return 'error';
+        }
+    }
+
+    return {
+        retrieve,
+        store
+    };
+}
+
+export default useSepoliaTestContract;
